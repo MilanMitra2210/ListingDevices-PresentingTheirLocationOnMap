f386102aa41f12b4adc48a6f5c67f8ea8454b1f4
MilanMitra2210 <milanmitranda2020@gmail.com>
2024-01-31 11:07:35 +0530

    sidebar Designed

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
